62429ed4bfd10e269e7338cacafe52e840acc82d
ithaloaugusto <Jacobinaithalo@gmail.com>
2025-10-25 21:56:04 -0300

    implementacoes realizadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia!");
+
+Smartphone nokia = new Nokia(numero: "123456789",modelo: "Nokia", imei: "123456789", memoria:32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("/n");
+
+Console.WriteLine("Smartphone Iphone!");
+
+Smartphone iphone = new Iphone(numero: "123456789",modelo: "Iphone", imei: "123456789", memoria:32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
